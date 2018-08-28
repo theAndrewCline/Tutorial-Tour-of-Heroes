81ed7b90e49b55dcaca56fc3b833f51b7deebee9
Andrew Cline <andrew.cline77@gmail.com>
2018-08-28 15:18:44 -0500

    add update operation for hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,6 +6,10 @@ import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Hero } from './Hero'
 import { MessageService } from './message.service'
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+}
+
 @Injectable({
   providedIn: 'root',
 })
@@ -32,6 +36,13 @@ export class HeroService {
     return this.http.get<Hero>(url).pipe(
       tap(_ => this.log(`fetched hero id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
+    )
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+      tap(_ => this.log(`update hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
     )
   }
 
