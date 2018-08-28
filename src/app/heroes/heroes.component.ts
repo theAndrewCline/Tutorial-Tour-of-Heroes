beb9b036f42c48ebf13ee1b80c3c6148c76a94e3
Andrew Cline <andrew.cline77@gmail.com>
2018-08-28 15:29:58 -0500

    add create operation for hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,12 +9,21 @@ import { HeroService } from '../hero.service'
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[]
+  constructor(private heroService: HeroService) {}
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes))
   }
 
-  constructor(private heroService: HeroService) {}
+  add(name: string): void {
+    name = name.trim()
+    if (!name) {
+      return
+    }
+    this.heroService.addHero({ name } as Hero).subscribe(hero => {
+      this.heroes.push(hero)
+    })
+  }
 
   ngOnInit() {
     this.getHeroes()
